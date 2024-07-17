662b8956d860e962b30c36a1adb29b63e39741e1
Shin Dong Won <eastorigin21@gmail.com>
2024-07-17 10:59:18 +0900

    5일차 강의 내용

---
+++ src/DentalFloss.java
@@ -0,0 +1,43 @@
+
+public class DentalFloss {
+	
+	/**
+	 * 치실 두께
+	 */
+	double thickness;
+	
+	/**
+	 * 치실 향
+	 */
+	String scent;
+	
+	/**
+	 * 치실 길이
+	 */
+	double length;
+	
+	/**
+	 * 치실 재질
+	 */
+	String material;
+	
+	/**
+	 * 구강 청결
+	 */
+	public void cleanTeeth() {
+		
+	}
+	/**
+	 * 구취 예방
+	 */
+	public void preventBadBreath() {
+		
+	}
+	
+	/**
+	 * 잇몸 건강 개선
+	 */
+	public void improveGumHealth() {
+		
+	}
+}
---
+++ src/Purifier.java
@@ -0,0 +1,71 @@
+
+public class Purifier {
+
+	/**
+	 * 정수기에 남은 물의 양 (단위: liter)
+	 */
+	double remainWater;
+	
+	/**
+	 * 물 배출 버튼
+	 */
+	String waterExhaustButton;
+	
+	/**
+	 * 냉수/온수 선택 버튼 (냉수/온수 여부)
+	 * 0: 냉수
+	 * 1: 온수
+	 */
+	int temperature;
+	
+	/**
+	 * 정수기에 남아있는 얼음의 양 (단위: 개)
+	 */
+	double remainIceBlock;
+	
+	/**
+	 * 얼음 배출 버튼
+	 */
+	String iceExhaustButton;
+	
+	/**
+	 * 정수 필터
+	 */
+	String filter;
+	
+	/**
+	 * 물 배출
+	 */
+	public void waterExhaust() {
+		
+	}
+	
+	/**
+	 * 온도 조절
+	 */
+	public void chooseTemperatrue() {
+		
+	}
+	
+	/**
+	 * 냉각 기능
+	 * (얼음을 생성)
+	 */
+	public void makeIce() {
+		
+	}
+	
+	/**
+	 * 정수 기능
+	 */
+	public void filtering() {
+		
+	}
+	
+	/**
+	 * 얼음 배출
+	 */
+	public void iceExhaust() {
+		
+	}
+}
